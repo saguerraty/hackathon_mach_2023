e5e891431dc653b8471d63a82972897fd5ba4e78
saguerraty <31260972+saguerraty@users.noreply.github.com>
2023-01-21 14:04:29 -0300

    added api to lambda mods

--- mvp/main.py
+++ mvp/main.py
@@ -1,17 +1,30 @@
 # main.py
 
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
 
-@app.get("/user_info/{user_id}")
-async def user_info(user_id: int):
+@app.get("/user_info/{user_id}/{user_request}")
+async def user_info(user_id: int, user_request: str):
+    model_url = "https://6dky2c4nx4.execute-api.us-east-1.amazonaws.com/default/openLineageWorker"
+    model_payload = {
+        "user_id": user_id,
+        "text" : user_request
+    }
+
+    model_response = requests.request("POST", model_url, json=model_payload)
+    content = model_response.json()
+    {"user_id": 54,
+      "user_class": "Experto en delivery",
+        "most_spent": ["Jumbo", "Uber", "CamiloCorp", "SebaInc"],
+                        "expected_next_spent": 100000, "rich_text": "En el a\u00f1o pasado esta ha sido tu historia:"}
     return {
         "user_id": user_id,
-        "data": {
-        "user_avg_spend": 100,
-        }
-        }
+        "most_spent": content["most_spent"],
+        "expected_spend" : content["expected_next_spent"],
+        "prompt_responde": content["rich_text"]
+    }
 
 @app.get("/user_groups/{user_id}")
 async def user_groups(user_id: int):
@@ -20,7 +33,3 @@ async def user_groups(user_id: int):
         "data": {
         "user_group": "Hello World"} 
     }
-
-@app.get("/user_spend/{user_id}")
-async def user_spend(user_id: int):
-    return {"user_id": user_id, "data": {"user_total_spend": 100}}
